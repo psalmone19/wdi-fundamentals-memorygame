0d8260acc35200ce4bd160d8c7cdeb04a837b6a4
Egan Bernardino <eegun19@yahoo.com>
2016-06-18 17:58:06 -0700

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,13 @@
-// console.log("JS file is connected to HTML! Woo!")
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
+ console.log("JS file is connected to HTML! Woo!")
+ var cardOne = "queen";
+ var cardTwo = "queen";
+ var cardThree = "king";
+ var cardFour = "king";
 
-
+if (cardOne === cardTwo) {
+  alert("You found a match!");
+} else if (cardThree === cardFour) {
+  alert ("You found a match!");
+} else {
+  alert ("Sorry, try again.");
+}
